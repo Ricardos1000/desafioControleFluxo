effb551e13fddf9fb670c728570e3484810b0ca5
Ricardos1000 <ricardo@connexaoinformatica.com.br>
2024-05-19 18:15:58 -0300

    Desafio Controle de Fluxo

---
+++ src/com/jurandir/domain/Contador.java
@@ -0,0 +1,40 @@
+package com.jurandir.domain;
+
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		terminal.close();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException pie) {
+			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println("Não é possivel fazer a contagem! parâmetro um maior que parâmetro dois");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if(parametroUm > parametroDois){
+			throw new ParametrosInvalidosException();
+			
+		}
+		
+		int contagem = parametroDois - parametroUm;
+		//realizar o for para imprimir os números com base na variável contagem
+		for(int x=0;x < contagem;x++) {
+			System.out.println("Imprimindo no número "+(x+1));
+		}
+	}
+}
---
+++ src/com/jurandir/domain/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package com.jurandir.domain;
+
+
+public class ParametrosInvalidosException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+}
